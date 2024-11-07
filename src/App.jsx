cd377b01a43ba66c75744f2e913fe4156a6e248d
ZAfifa15 <147030759+ZAfifa15@users.noreply.github.com>
2024-11-07 18:26:29 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>COLLAB</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count}collab
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
