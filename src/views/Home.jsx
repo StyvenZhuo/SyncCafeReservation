cee1b102557b57a86c5bd05f0b5b418200b729e2
StyvenZhuo <151433246+StyvenZhuo@users.noreply.github.com>
2024-11-14 22:00:48 +0700

    -

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,12 @@
-import React from 'react'
+import React from 'react';
+
 
 function Home() {
   return (
-    <div class='font-xl'>
+    <div class="h-screen w-full bg-gray-500 flex items-center justify-center">
 
-    <div class='flex flex-row-0 flex-wrap'>
-      <div>kiri</div>
-      <div>kanan</div>
     </div>
-    
-    
-    
-    </div>
+
   )
 }
 
