a4db9506eabe8ffb84d06d79adf9bc8e373be1de
snowyish <90228920+snowyish@users.noreply.github.com>
2024-11-25 15:26:40 +0700

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -250,7 +250,7 @@ function Home() {
 
       {/* Login/Sign-Up Popup */}
       {showLoginPopup && (
-        <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
+        <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50" onClick={() => setShowLoginPopup(false)}>
           <div className="bg-white p-8 rounded-xl shadow-xl w-full sm:w-96 md:w-[500px]">
             <h2 className="text-2xl font-semibold mb-4">
               {isSignUp ? "Sign Up" : "Log In"}
